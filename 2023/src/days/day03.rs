828ba17d5ccdf1af63c59137e29719eb70dbe3fb
luk6xff <lukasz.uszko@gmail.com>
2024-01-05 00:34:33 +0100

    day03

--- 2023/src/days/day03.rs
+++ 2023/src/days/day03.rs
@@ -1,6 +1,13 @@
 use super::day::Day;
 
 
+#[derive(Debug)]
+enum Value {
+    Empty,
+    Symbol(char),
+    Number(u32),
+}
+
 pub struct Day03 {}
 
 impl Day for Day03 {
@@ -9,15 +16,33 @@ impl Day for Day03 {
     }
 
     fn part_1(&self) -> String {
-        let res: usize = self.load_input()
-                                .lines();
-
-        res.to_string()
+        let res: Vec<(usize, usize, Value)> = self.load_input()
+                            .lines()
+                            .enumerate()
+                            .flat_map(|(y, line)| {
+                                line.chars()
+                                    .enumerate()
+                                    .map( move |(x, c)| {
+                                        match c {
+                                            '.' => (x, y, Value::Empty),
+                                            character => if character.is_ascii_digit() {
+                                                (x, y, Value::Number(character.to_digit(10).expect("Invalid digit"),))
+                                            } else {
+                                                (x, y, Value::Symbol(character))
+                                            },
+                                            symbol => (x, y, Value::Symbol(symbol)),
+                                        }
+                                    })
+                            }).collect::<Vec<(usize, usize, Value)>>();
+        for (x, y, v) in res {
+            println!("{} {} {:?}", x, y, v);
+        }
+        " ".to_string()
     }
 
     fn part_2(&self) -> String {
-        let res: u32 = self.load_input()
-                                .lines();
-        res.to_string()
+        // let res: u32 = self.load_input()
+        //                         .lines();
+        " ".to_string()
     }
 }
--- 2023/src/days/mod.rs
+++ 2023/src/days/mod.rs
@@ -1,3 +1,4 @@
 pub mod day;
 pub mod day01;
 pub mod day02;
+pub mod day03;
